5164bb0f2741158652a542e96f82ee5be765ac4d
Vineet Kumar <vineet.vinayakumar@gmail.com>
2020-01-22 19:02:41 +0530

    mongoose updated

--- tests/math.test.js
+++ tests/math.test.js
@@ -36,10 +36,9 @@ test('Should add two numbers', (done) => {
     add(2,3).then((sum) => {
         expect(sum).toBe(5)
         done()
+    })  
+    test('Should add two numbers async/await', async () => {
+        const sum = await add(10, 22)
+        expect(sum).toBe(32)
     })
-})
-
-test('Should add two numbers async/await', async () => {
-    const sum = await add(10, 22)
-    expect(sum).toBe(32)
 })
